77849f0f435ce2529513226148228a4be3d808d5
Anand Verma <anandverma552@gmail.com>
2024-08-05 17:29:47 -0400

    Adding Room table and migration

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import uuid
 # Create your models here.
 
 class Room(models.Model):
@@ -10,6 +10,7 @@ class Room(models.Model):
     #participants = models.
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     def __str__(self) -> str:
         return self.name
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,30 +1,28 @@
 
 from django.shortcuts import render
+from .models import Room
 
+# roomsList = [
+#     {
+#         'id': 1, 'name' : 'Lets learn Django'
+#     },
+#      {
+#         'id': 2, 'name' : 'Lets learn Javascript'
+#     },
+#      {
+#         'id': 3, 'name' : 'Back End Development'
+#     },
 
-roomsList = [
-    {
-        'id': 1, 'name' : 'Lets learn Django'
-    },
-     {
-        'id': 2, 'name' : 'Lets learn Javascript'
-    },
-     {
-        'id': 3, 'name' : 'Back End Development'
-    },
-
-]
+# ]
 
 def home(request):
+    roomsList = Room.objects.all()
+    print(roomsList)
     context = {'roomsList' : roomsList}
     return render(request, 'base/home.html', context)
 
-def room(request, pk):
-    selectedRoom = None
-    for i in roomsList:
-        if i['id'] == int(pk):
-            selectedRoom = i
-
+def room(request, pk):   
+    selectedRoom = Room.objects.get(id=pk)   
     context = {'selectedRoom' : selectedRoom}
     return render(request, 'base/room.html', context)
 
