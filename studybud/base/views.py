4fade766a4989fdfe09be36779050c6dfb54a700
Anand Verma <anandverma552@gmail.com>
2024-08-05 17:09:50 -0400

    setting up migrations

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,9 +1,30 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
 
-def rooms(request):
-    return render(request, 'room.html')
+roomsList = [
+    {
+        'id': 1, 'name' : 'Lets learn Django'
+    },
+     {
+        'id': 2, 'name' : 'Lets learn Javascript'
+    },
+     {
+        'id': 3, 'name' : 'Back End Development'
+    },
+
+]
+
+def home(request):
+    context = {'roomsList' : roomsList}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    selectedRoom = None
+    for i in roomsList:
+        if i['id'] == int(pk):
+            selectedRoom = i
+
+    context = {'selectedRoom' : selectedRoom}
+    return render(request, 'base/room.html', context)
 
